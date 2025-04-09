4f7bd5e33c29cb7c13d9df4146e2fd45fb41e908
Cathy <45395549+CathyDolle@users.noreply.github.com>
2025-04-09 18:36:36 +0200

    added menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,8 @@
 import { Content } from "@/ui/components";
 import { Storytelling } from "@/ui/components/storytelling/Storytelling";
+import menu from "@/assets/images/menu.jpg";
+import Image from "next/image";
 
-import landscape from "@/assets/images/content/i3.jpeg";
 interface StorytellingItem {
   variant: "portrait" | "landscape";
   mediaType: "image" | "video";
@@ -10,43 +11,12 @@ interface StorytellingItem {
   description: string | React.ReactNode;
 }
 
-const storytellingItems: StorytellingItem[] = [
-  {
-    variant: "landscape",
-    mediaType: "video",
-    media: "/videos/v1.MOV",
-    title: "MENU sur-mesure",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi temporibus blanditiis nam repellendus enim iure ducimus. Itaque quae",
-  },
-  {
-    variant: "portrait",
-    mediaType: "video",
-    media: "/videos/v5.MOV",
-    title: "Desc 2커피",
-    description: (
-      <>
-        11 rue des Écouffes,
-        <br />
-        Paris, France 75004
-      </>
-    ),
-  }
-];
+
 
 export default function Menu() {
   return (
     <Content className="lg:span-gap-1-wider gap-40 mb-64 text-12 leading-near">
-      {storytellingItems.map((item, index) => (
-        <Storytelling
-          key={index}
-          variant={item.variant}
-          mediaType={item.mediaType}
-          media={item.media}
-          title={item.title}
-          description={item.description}
-        />
-      ))}
+      <Image className="w-full" src={menu} alt="menu" />
     </Content>
   );
 }
